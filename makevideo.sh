2bb6f8d034daae9db31672e287c92c5717bd04ef
MaximBrewer <pimax1978@icloud.com>
2020-12-17 22:31:25 +0600

    df

--- makevideo.sh
+++ makevideo.sh
@@ -23,11 +23,11 @@ if (($dw < 0)); then
     fi
 fi
 
-convert $1 -crop $(($w - ($w * $crw / 100)))x$(($h - ($h * $crh / 100)))+$(($w * $crw / 100 / 2))+$(($h * $crh / 100 / 2)) "tmp.png"
+convert $1 -crop $(($w - ($w * $crw / 100)))x$(($h - ($h * $crh / 100)))+$(($w * $crw / 100 / 2))+$(($h * $crh / 100 / 2)) $1/tmp/orders/$2/tmp.png
 
-convert "tmp.png" -scale 660X880 -bordercolor "rgba(0,0,0,0.4)" -border 1 "tmp.png"
+convert $1/tmp/orders/$2/tmp.png -scale 660X880 -bordercolor "rgba(0,0,0,0.4)" -border 1 $1/tmp/orders/$2/tmp.png
 
-read wn hn < <(identify -format "%w %h" "tmp.png")
+read wn hn < <(identify -format "%w %h" "$1/tmp/orders/$2/tmp.png")
 
 wdn=$((720 + (660 - $wn) / 2))
 hdn=$((115 + (880 - $hn) / 2))
@@ -35,15 +35,15 @@ hdn=$((115 + (880 - $hn) / 2))
 echo $wdn
 echo $hdn
 
-convert -background None -virtual-pixel transparent -background transparent \
-    \( "tmp.png" +distort Perspective '0,0 0,0  880,10 880,-10  880,630 880,680  0,660 0,660' \) \
-    "perspective.png"
+convert -background none -virtual-pixel transparent -background transparent \
+    \( $1/tmp/orders/$2/tmp.png +distort Perspective '0,0 0,0  880,10 880,-10  880,630 880,680  0,660 0,660' \) \
+    $1/tmp/orders/$2/perspective.png
 
-convert "perspective.png"  -background transparent -rotate 1 "rotate.png"
+convert $1/tmp/orders/$2/perspective.png  -background transparent -rotate 1 $1/tmp/orders/$2/rotate.png
 
-composite -geometry '+'$wdn'+'$hdn "rotate.png" /home/debian/www/storage/tmp/photo.png /home/debian/www/storage/tmp/mask.jpg "final.jpg"
+composite -geometry '+'$wdn'+'$hdn $1/tmp/orders/$2/rotate.png $1/tmp/orders/$2/photo.png $1/tmp/mask.jpg $1/tmp/orders/$2/final.jpg
 
-jpegoptim "final.jpg" --strip-all
+jpegoptim $1/tmp/orders/$2/final.jpg --strip-all
 
-ffmpeg -y -hide_banner -loglevel panic -filter_complex aevalsrc=0 -loop 1 -i "final.jpg" -t 5.8 "final.mp4"
-ffmpeg -y -hide_banner -loglevel panic -i "final.mp4" -i /home/debian/www/storage/tmp/sound.aac -c:a copy -c:v libx264 -map 0:v:0 -map 1:a:0 "final.ts"
+ffmpeg -y -hide_banner -loglevel panic -filter_complex aevalsrc=0 -loop 1 -i $1/tmp/orders/$2/final.jpg -t 5.8 $1/tmp/orders/$2/final.mp4
+ffmpeg -y -hide_banner -loglevel panic -i $1/tmp/orders/$2/final.mp4 -i $1/tmp/sound.aac -c:a copy -c:v libx264 -map 0:v:0 -map 1:a:0 $1/tmp/orders/$2/final.ts
