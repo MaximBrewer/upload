31a6e0f19ffcee6ac0d4be39a286fef25db89c7d
MaximBrewer <pimax1978@icloud.com>
2020-12-17 23:27:22 +0600

    sds

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -19,10 +19,9 @@ class PhotoController extends Controller
 
 
         if (!is_file($ts_path)) {
-            exec("./makevideo.sh " . $photo_path . " " . $order_id . " " . storage_path());
+            exec("../makevideo.sh " . $photo_path . " " . $order_id . " " . storage_path());
         }
-        echo("./makevideo.sh " . $photo_path . " " . $order_id . " " . storage_path());
-
+        
         if (is_file($ts_path)) {
             $client = new \GuzzleHttp\Client();
             $response = $client->post('https://auth.platformcraft.ru/token', [
