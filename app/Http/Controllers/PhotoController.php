f55733a269ee6148f8503b9066c4b4e444c5fcdc
MaximBrewer <pimax1978@icloud.com>
2020-12-24 22:58:01 +0600

    sdsd

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -10,47 +10,57 @@ class PhotoController extends Controller
     {
         if (!$request->get("order") || !$request->file('photo')) return false;
         @mkdir(storage_path("tmp/orders/" . $request->get("order")), 0777, true);
-        $photo_path = storage_path("tmp/orders/" . $request->get("order") . "/photo." . $request->photo->extension());
-        $order_id = $request->get("order");
 
-        $url = "https://montage-cache.cdnvideo.ru/montage/photo/" . $order_id . ".ts";
-        $headers = @get_headers($url);
-        echo $headers[0] . PHP_EOL;
-        if (strpos($headers[0], '200')) 
-            return false;
+        touch(storage_path("tmp/orders/" . $request->get("order") . "/lock.txt"));
 
-        file_put_contents($photo_path, file_get_contents($request->file('photo')));
+        $fp = fopen(storage_path("tmp/orders/" . $request->get("order") . "/lock.txt"), 'r+');
+        if (flock($fp, LOCK_EX | LOCK_NB)) {
 
-        $ts_path = storage_path("tmp/orders/" . $request->get("order") . "/final.ts");
+            $photo_path = storage_path("tmp/orders/" . $request->get("order") . "/photo." . $request->photo->extension());
+            $order_id = $request->get("order");
 
-        if (!is_file($ts_path)) {
-            exec("../makevideo.sh " . $photo_path . " " . $order_id . " " . storage_path());
-        }
+            $url = "https://montage-cache.cdnvideo.ru/montage/photo/" . $order_id . ".ts";
+            $headers = @get_headers($url);
+            echo $headers[0] . PHP_EOL;
+            if (strpos($headers[0], '200'))
+                return false;
 
-        if (is_file($ts_path)) {
-            $client = new \GuzzleHttp\Client();
-            $response = $client->post('https://auth.platformcraft.ru/token', [
-                'form_params' => ['login' => 'montage', 'password' => 'fz7skpFa']
-            ]);
-            $array = json_decode($response->getBody()->getContents());
-            $client->request('POST', "https://filespot.platformcraft.ru/2/fs/container/" . $array->user_id . "/object/photo/" . $order_id . ".ts", [
-                'multipart' => [
-                    [
-                        'name'     => 'file',
-                        'contents' => fopen($ts_path, "r"),
-                        'filename' => $order_id . ".ts"
+            file_put_contents($photo_path, file_get_contents($request->file('photo')));
+            $ts_path = storage_path("tmp/orders/" . $request->get("order") . "/final.ts");
+
+
+
+            if (!is_file($ts_path)) {
+                exec("../makevideo.sh " . $photo_path . " " . $order_id . " " . storage_path());
+            }
+
+            if (is_file($ts_path)) {
+                $client = new \GuzzleHttp\Client();
+                $response = $client->post('https://auth.platformcraft.ru/token', [
+                    'form_params' => ['login' => 'montage', 'password' => 'fz7skpFa']
+                ]);
+                $array = json_decode($response->getBody()->getContents());
+                $client->request('POST', "https://filespot.platformcraft.ru/2/fs/container/" . $array->user_id . "/object/photo/" . $order_id . ".ts", [
+                    'multipart' => [
+                        [
+                            'name'     => 'file',
+                            'contents' => fopen($ts_path, "r"),
+                            'filename' => $order_id . ".ts"
+                        ],
                     ],
-                ],
-                'headers' => [
-                    "Authorization" => "Bearer " . $array->access_token
-                ]
-            ]);
-            @unlink(storage_path('app/public/orders/' . $order_id . '/') . "tmp.png");
-            @unlink(storage_path('app/public/orders/' . $order_id . '/') . "perspective.png");
-            @unlink(storage_path('app/public/orders/' . $order_id . '/') . "rotate.png");
-            @unlink(storage_path('app/public/orders/' . $order_id . '/') . "final.jpg");
-            @unlink(storage_path('app/public/orders/' . $order_id . '/') . "final.mp4");
-            @unlink(storage_path('app/public/orders/' . $order_id . '/') . "final.ts");
+                    'headers' => [
+                        "Authorization" => "Bearer " . $array->access_token
+                    ]
+                ]);
+                @unlink(storage_path('app/public/orders/' . $order_id . '/') . "tmp.png");
+                @unlink(storage_path('app/public/orders/' . $order_id . '/') . "perspective.png");
+                @unlink(storage_path('app/public/orders/' . $order_id . '/') . "rotate.png");
+                @unlink(storage_path('app/public/orders/' . $order_id . '/') . "final.jpg");
+                @unlink(storage_path('app/public/orders/' . $order_id . '/') . "final.mp4");
+                @unlink(storage_path('app/public/orders/' . $order_id . '/') . "final.ts");
+            }
+            fclose($fp);
+            return 0;
         }
     }
 }
