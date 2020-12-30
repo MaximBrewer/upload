1f65e7ba3f366e0809dbd2eb65f7c5a9f9157552
MaximBrewer <pimax1978@icloud.com>
2020-12-30 20:31:55 +0600

    sdsd

---
+++ app/Console/Commands/Cron.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Cron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:exec';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+
+        foreach (glob("/mnt/vol1/www/mp4/*.mp4") as $filename) {
+            echo time() - filemtime($filename);
+        }
+
+
+        return 0;
+    }
+}
